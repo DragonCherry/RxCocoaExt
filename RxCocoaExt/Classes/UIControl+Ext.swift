27f67715b75dfd5459b96aea9946071c04226f77
DragonCherry <dragoncherry@naver.com>
2017-07-27 11:15:42 +0900

    added isEnabled control property

---
+++ RxCocoaExt/Classes/UIControl+Ext.swift
@@ -0,0 +1,80 @@
+//
+//  UIControl+Ext.swift
+//  Pods
+//
+//  Created by DragonCherry on 7/13/17.
+//
+//
+
+import RxSwift
+import RxCocoa
+
+// MARK: - UIBindingObserver for UIControl.isFirstResponder
+extension Reactive where Base: UIControl {
+    
+    public var isFirstResponder: ControlProperty<Bool> {
+        
+        func property<T, ControlType: UIControl>(_ control: ControlType, getter:  @escaping (ControlType) -> T, setter: @escaping (ControlType, T) -> ()) -> ControlProperty<T> {
+            let values: Observable<T> = Observable.deferred { [weak control] in
+                guard let existingSelf = control else {
+                    return Observable.empty()
+                }
+                
+                return existingSelf.rx.controlEvent([.editingDidBegin, .editingDidEnd, .editingDidEndOnExit])
+                    .flatMap { _ in
+                        return control.map { Observable.just(getter($0)) } ?? Observable.empty()
+                    }
+                    .startWith(getter(existingSelf))
+            }
+            return ControlProperty(values: values, valueSink: UIBindingObserver(UIElement: control) { control, value in
+                setter(control, value)
+            })
+        }
+        
+        return property(
+            self.base,
+            getter: { control in
+                control.isFirstResponder
+            },
+            setter: { control, value in
+                if control.isFirstResponder != value {
+                    _ = value ? control.becomeFirstResponder() : control.resignFirstResponder()
+                }
+            })
+    }
+}
+
+// MARK: - UIBindingObserver for UIControl.isEnabled
+extension Reactive where Base: UIControl {
+    
+    public var isEnabled: ControlProperty<Bool> {
+        
+        func property<T, ControlType: UIControl>(_ control: ControlType, getter:  @escaping (ControlType) -> T, setter: @escaping (ControlType, T) -> ()) -> ControlProperty<T> {
+            let values: Observable<T> = Observable.deferred { [weak control] in
+                guard let existingSelf = control else {
+                    return Observable.empty()
+                }
+                
+                return existingSelf.rx.isEnabled
+                    .flatMap { _ in
+                        return control.map { Observable.just(getter($0)) } ?? Observable.empty()
+                    }
+                    .startWith(getter(existingSelf))
+            }
+            return ControlProperty(values: values, valueSink: UIBindingObserver(UIElement: control) { control, value in
+                setter(control, value)
+            })
+        }
+        
+        return property(
+            self.base,
+            getter: { control in
+                control.isEnabled
+        },
+            setter: { control, value in
+                if control.isEnabled != value {
+                    control.isEnabled = value
+                }
+        })
+    }
+}
